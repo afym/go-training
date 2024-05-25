f180b4c0b1ef0e1756cb24fa98a4faeaed649304
Angel <2775198+afym@users.noreply.github.com>
2024-05-25 18:54:23 -0500

    Update bubble.go

--- ordering/bubble.go
+++ ordering/bubble.go
@@ -2,18 +2,16 @@ package ordering
 
 func BubbleSort(numbers []int) []int {
 	size := len(numbers)
-	counter := size
 
 	for i := 0; i < size; i++ {
 		swapped := false
-		for j := 0; j < counter; j++ {
-			if (counter > j+1) && numbers[j] > numbers[j+1] {
+
+		for j := 0; j < size-i-1; j++ {
+			if numbers[j] > numbers[j+1] {
 				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
 				swapped = true
 			}
 		}
-
-		counter -= 1
 
 		if !swapped {
 			break
